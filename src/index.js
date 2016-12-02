5111de2f0d36f3c39bb775e42867053686b93058
Geoff Kimball <geoff@geoffkimball.com>
2016-12-01 23:01:27 -0800

    Allow custom components to be passed to <Markdown /> component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React, { PropTypes } from 'react';
+import assign from 'lodash.assign';
 import markdownIt from 'markdown-it';
 import ast from './util/ast';
 import renderTokens from './util/renderTokens';
-import components from './components';
+import defaultComponents from './components';
 
 /**
  * Renders React components from a Markdown string.
  * @todo Allow for custom components.
  */
-export default function Markdown({ input, renderer }) {
+export default function Markdown({ input, renderer, components = {} }) {
   const { nodes } = ast(renderer.parse(input));
 
   return (
     <div>
-      {renderTokens(nodes, components)}
+      {renderTokens(nodes, assign({}, defaultComponents, components))}
     </div>
   );
 }
@@ -23,10 +24,12 @@ export default function Markdown({ input, renderer }) {
  * @type Object
  * @prop {String} input - Markdown string.
  * @prop {Object} [renderer] - markdown-it instance.
+ * @prop {Object.<String, Class>} [components] - Custom components.
  */
 Markdown.propTypes = {
   input: PropTypes.string.isRequired,
   renderer: PropTypes.object,
+  components: PropTypes.objectOf(PropTypes.func),
 }
 
 /**
